10ccd5007c4c17f363a3bd723d1736821c5e236a
Trayshawn <traytwebb14@yahoo.com>
2020-11-19 18:43:36 -0600

    commit tree 5011f6023e876268f69db552f69e166a09049809

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,39 @@
 # Add your code here
+
+require "pry"
+
+class Dog
+
+    attr_accessor :name, :save
+
+    @@all = []
+
+
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end 
+
+    def self.print_all
+        @@all.collect do |p| 
+        puts "#{p.name}" 
+        end 
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.clear_all
+        @@all.clear
+    end 
+
+
+
+
+end
